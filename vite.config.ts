ec81267e6913165a23049fcdde8c76e6dfebf987
Eyre_S <sukazyo@outlook.com>
2024-05-05 22:28:59 +0800

    added ann's toolchain

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,34 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import stylusAlias from "vite-plugin-stylus-alias-next";
+import path from "node:path";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+	
+	plugins: [
+		remix({
+			ignoredRouteFiles: [
+				"**/*.css",
+				"**/*.styl",
+				"**/*.stylus",
+			]
+		}),
+		tsconfigPaths(),
+		stylusAlias(),
+	],
+	
+	resolve: {
+		alias: {
+			"~": path.resolve(__dirname, "./app"),
+		}
+	},
+	css: {
+		modules: {
+			localsConvention: "camelCase"
+		}
+	}
+	
 });
